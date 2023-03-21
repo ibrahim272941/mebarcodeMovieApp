df64a72eae18cc26a77271ed724641e92baf0e1a
Ibrahim Coban <ibrahimcoban272941@gmail.com>
2023-03-22 00:07:53 +0100

    cast deatils

--- src/pages/CastDetails/CastDetails.js
+++ src/pages/CastDetails/CastDetails.js
@@ -6,37 +6,43 @@ export const CastDetails = () => {
   const { personBio, personMovie } = useContext(CastContext);
   console.log(personMovie);
   return (
-    <div className="castDetails">
-      <div className="personTop">
-        <img
-          className="personImg"
-          src={`${IMG_API}${personBio.profile_path}`}
-          alt=""
-        />
-        <p className="personName">{personBio.name}</p>
-        <p className="personBio">{personBio.biography}</p>
-      </div>
-
-      <div className="filmography">
-        {personMovie?.map((item, i) => (
-          <div key={i} className="castingContainer">
-            {item.poster_path ? (
-              <>
-                <img
-                  className="personCastingImg"
-                  src={`${IMG_API}${item.poster_path}`}
-                  alt=""
-                />
-
-                <div className="castinFilmInfo">
-                  <p className="castingFilmName">{item.original_title}</p>
-                  <p className="castingName">{item.character}</p>
-                </div>
-              </>
-            ) : null}
+    <>
+      {personBio && personMovie ? (
+        <div className="castDetails">
+          <div className="personTop">
+            <img
+              className="personImg"
+              src={`${IMG_API}${personBio.profile_path}`}
+              alt=""
+            />
+            <p className="personName">{personBio.name}</p>
+            <p className="personBio">{personBio.biography}</p>
           </div>
-        ))}
-      </div>
-    </div>
+
+          <div className="filmography">
+            {personMovie?.map((item, i) => (
+              <div key={i} className="castingContainer">
+                {item.poster_path ? (
+                  <>
+                    <img
+                      className="personCastingImg"
+                      src={`${IMG_API}${item.poster_path}`}
+                      alt=""
+                    />
+
+                    <div className="castinFilmInfo">
+                      <p className="castingFilmName">{item.original_title}</p>
+                      <p className="castingName">{item.character}</p>
+                    </div>
+                  </>
+                ) : null}
+              </div>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <p>Please Wait</p>
+      )}
+    </>
   );
 };
